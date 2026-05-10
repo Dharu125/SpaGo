1cb0c8bcbca99a2676cbf025a55aefb59721053c
Dharu125 <dharudwisatria125@gmail.com>
2026-05-10 11:48:00 +0800

    insyallah tinggal bikin database

--- codingan/booking.php
+++ codingan/booking.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SpaGo - Booking</title>
-    <link rel="stylesheet" href="styles.css" />
-</head>
 <body>
     <section id="booking" class="section">
       <div class="container booking">
